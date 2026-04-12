cb43ae20656620db45bfbccd4f1c65ef19ce1ee6
Aldwin Joshua Cunanan <aldwinjoshua.cunanan22@gmail.com>
2026-04-12 12:37:48 -0500

    file structure for adopter resources and build interface

---
+++ src/api/v1/adopters/adopters.interface.ts
@@ -0,0 +1,10 @@
+export interface Adopter {
+  id?: string;
+  name: string;
+  email: string;
+  phone: string;
+  address?: string;
+  preferredSpecies?: string[]; // for subscription feature
+  createdAt: string;
+  updatedAt: string;
+}
